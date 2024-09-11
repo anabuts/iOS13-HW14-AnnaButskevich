cb5a92fa0c0ffa90b9b6fc62c96b8b94a8346848
anabuts <ana.butskevich@gmail.com>
2024-09-11 17:56:15 +0300

    Initial Commit

---
+++ iOS13-HW14-AnnaButskevich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS13-HW14-AnnaButskevich
+//
+//  Created by Анна Буцкевич on 11.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
